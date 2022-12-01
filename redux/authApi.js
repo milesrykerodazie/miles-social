76ffe66fbc733d11f1a11e174ea445b7f4550c20
milesrykerodazie <milesodazie@gamil.com>
2022-12-01 09:23:40 +0100

    update login api

--- redux/authApi.js
+++ redux/authApi.js
@@ -1,9 +1,9 @@
-import axios from "axios";
+import axios from 'axios';
 
 const API = axios.create({
-  baseURL: "https://milessocial-media-backend.herokuapp.com/",
+   baseURL: 'https://web-production-cd53.up.railway.app/',
 });
 
-export const login = (loginData) => API.post("milesapi/login", loginData);
+export const login = (loginData) => API.post('milesapi/login', loginData);
 export const register = (registerData) =>
-  API.post("milesapi/register", registerData);
+   API.post('milesapi/register', registerData);
